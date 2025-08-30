7d7ca7a5a055cb59b21acb9567a9a041ef1d9eeb
atit <atit@gmail.com>
2025-08-31 00:42:57 +0700

    Change Engine

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import add from '../src/index.js';
+import { add } from '../src/index.js';
 
 describe('add function', () => {
   it('should return the sum of two numbers', () => {
